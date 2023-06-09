9a7a5d383eeff1e017b310dc5742354d979a8f4d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-09 17:44:26 +0530

    added test ninja class lib project

---
+++ Solution/TestNinja/Mocking/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestNinja.Mocking
+{
+    public class EmployeeController
+    {
+        public EmployeeController(IEmployeeStorage employeeStorage)
+        {
+            _employeeStorage = employeeStorage;
+        }
+
+        private readonly IEmployeeStorage _employeeStorage;
+
+        public ActionResult DeleteEmployee(int id)
+        {
+            _employeeStorage.DeleteEmployee(id);
+            return RedirectToAction("Employees");
+        }
+
+        private ActionResult RedirectToAction(string employees)
+        {
+            return new RedirectResult();
+        }
+    }
+
+    public class ActionResult { }
+
+    public class RedirectResult : ActionResult { }
+
+    public class EmployeeContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public void SaveChanges()
+        {
+        }
+    }
+
+    public class Employee
+    {
+    }
+}
