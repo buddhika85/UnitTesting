827b16d2ce461c786d7603feb62938ecd0927436
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-09 19:28:18 +0530

    Reservation class - CanBeCancelledBy method unit tests

--- Solution/TestNinja.UnitTests/ReservationTests.cs
+++ Solution/TestNinja.UnitTests/ReservationTests.cs
@@ -26,5 +26,33 @@ namespace TestNinja.UnitTests
             // assert
             Assert.That(result, Is.True);
         }
+
+        [Test]
+        public void CanBeCancelledBy_IsNotAdmin_ReturnsFalse()
+        {
+            // arrange
+            var nonAdmin = new User { IsAdmin = false };
+            Reservation.MadeBy = new User { IsAdmin = false };
+
+            // act
+            var result = Reservation.CanBeCancelledBy(nonAdmin);
+
+            // assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_IsMadeByUser_ReturnsTrue()
+        {
+            // arrange
+            var madebyUser = new User { IsAdmin = false };
+            Reservation.MadeBy = madebyUser;
+
+            // act
+            var result = Reservation.CanBeCancelledBy(madebyUser);
+
+            // assert
+            Assert.That(result, Is.True);
+        }
     }
 }
