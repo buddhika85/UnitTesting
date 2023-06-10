66d08a68e7a864306600a12a5027c7dba9e807dc
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-10 13:16:17 +0530

    Math add unit test

---
+++ Solution/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_PassTwoNums_ReturnSum()
+        {
+            // arrange
+            var math = new TestNinja.Fundamentals.Math();
+            var a = 1;
+            var b = 2;
+            var sum = a + b;
+
+            // act 
+            var result = math.Add(a, b);
+
+            // assert
+            Assert.That(result, Is.EqualTo(sum));
+        }
+    }
+}
