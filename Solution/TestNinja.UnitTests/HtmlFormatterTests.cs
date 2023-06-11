047fe752d4d9a838a9fe7c8bd2577c651dabc6ec
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-11 14:00:00 +0530

    HtmlFormatter Tests

---
+++ Solution/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,32 @@
+﻿using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        private HtmlFormatter _htmlFormatter;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _htmlFormatter = new HtmlFormatter();
+        }
+
+        [TestCase("Hello")]
+        [TestCase("World")]
+        [TestCase("Hello World")]
+        public void FormatAsBold_WhenCalled_ReturnBold(string input)
+        {
+            // arrange            
+            var expected = $"<strong>{input}</strong>";
+
+            // act
+            var actual = _htmlFormatter.FormatAsBold(input);
+
+            // assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
