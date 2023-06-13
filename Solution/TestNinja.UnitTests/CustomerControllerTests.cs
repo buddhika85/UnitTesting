7bec21ac3f32ab23c911d7f55434c2b41db48d3d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-13 17:57:48 +0530

    controllers action methods testsing

---
+++ Solution/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController _customerController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _customerController = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_IdZero_ReturnNotFound()
+        {
+            // arrange
+            var id = 0;
+
+            // act
+            var actionResult = _customerController.GetCustomer(id);
+
+            // assert
+            Assert.That(actionResult, Is.Not.Null);
+            Assert.That(actionResult is NotFound);
+        }
+
+        [Test]
+        public void GetCustomer_IdNonZero_ReturnOk()
+        {
+            // arrange
+            var id = 1;
+
+            // act
+            var actionResult = _customerController.GetCustomer(id);
+
+            // assert
+            Assert.That(actionResult, Is.Not.Null);
+            Assert.That(actionResult is Ok);
+        }
+    }
+}
