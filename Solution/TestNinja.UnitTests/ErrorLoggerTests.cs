dd2fa2169fbc3b36db038c464097cce744e35045
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-13 18:22:48 +0530

    test for void methods, exception throwing

---
+++ Solution/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,39 @@
+﻿using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger _errorLogger;
+
+        [SetUp]
+        public void Setup()
+        {
+            // arrange part for all tests in this class
+            _errorLogger = new ErrorLogger();
+        }
+
+        [Test]
+        [TestCase("testError", "testError")]
+        [TestCase("123", "123")]
+        public void Log_WhenCalledWithNonNullNonEmpty_ChangeLastError(string error, string expected)
+        {
+            // act
+            _errorLogger.Log(error);
+
+            // assert
+            Assert.That(_errorLogger.LastError, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("  ")]
+        public void Log_WhenCalledWithNullOrEmpty_ChangeLastError(string error)
+        {
+            Assert.That(() => _errorLogger.Log(error), Throws.ArgumentNullException);
+        }
+
+    }
+}
