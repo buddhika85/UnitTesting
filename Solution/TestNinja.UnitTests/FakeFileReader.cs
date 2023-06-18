3cf0d61fd3c78888b1e2aaee40dd2c3fe5fac29f
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-18 19:46:19 +0530

    added fake file reader class for mocking

---
+++ Solution/TestNinja.UnitTests/FakeFileReader.cs
@@ -0,0 +1,16 @@
+﻿using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            //return "[" +
+            //    "{id: 1, Title: \"abc\", IsProcessed: true}" +
+            //    "{id: 2, Title: \"def\", IsProcessed: false}" +
+            //    "]";
+            return "";
+        }
+    }
+}
--- Solution/TestNinja/Mocking/FileReader.cs
+++ Solution/TestNinja/Mocking/FileReader.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-
-namespace TestNinja.Mocking
+﻿namespace TestNinja.Mocking
 {
     public interface IFileReader
     {
