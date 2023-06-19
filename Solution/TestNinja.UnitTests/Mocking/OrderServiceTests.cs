f46d4739d1594a23c9c800fd0695cc8d396129d6
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-19 10:13:52 +0530

    place order test with mock

---
+++ Solution/TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+
+        [Test]
+        public void PlaceOrder_PassOrder_StoreAndReturnOrderId()
+        {
+            // arrange
+            var mockStorage = new Mock<IStorage>();
+            var mockOrder = new Mock<Order>();
+            mockStorage.Setup(x => x.Store(mockOrder.Object)).Returns(1);
+            var orderService = new OrderService(mockStorage.Object);
+
+            // act
+            var actualId = orderService.PlaceOrder(mockOrder.Object);
+
+            // assert
+            Assert.That(actualId, Is.EqualTo(1));
+        }
+    }
+}
