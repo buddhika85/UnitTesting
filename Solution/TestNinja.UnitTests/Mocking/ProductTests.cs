342ca2446b512ae9318b3a2fbc68138807e1fcb9
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-20 10:22:26 +0530

    Product Tests

---
+++ Solution/TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,30 @@
+﻿using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        private Product? _product;
+
+        [SetUp]
+        public void Setup()
+        {
+            _product = new Product { ListPrice = 100.0f };
+        }
+
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            // arrange 
+            var customer = new Customer { IsGold = true };
+
+            // act
+            var actual = _product?.GetPrice(customer);
+            var expected = _product?.ListPrice * 0.7f;
+
+            // assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
