3222a74b92eec531a6e9575e2e1aa923ef6b5d52
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-23 09:22:58 +0530

    DeleteEmployee_WhenIdPassed_ReturnsActionResult

--- Solution/TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
+++ Solution/TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -6,22 +6,46 @@ namespace TestNinja.UnitTests.Mocking
     [TestFixture]
     public class EmployeeControllerTests
     {
+        private readonly int _empId = 1;
+        private EmployeeController _mockEmployeeController = null!;
+        private Mock<IEmployeeStorage> _mockStorage = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockStorage = new Mock<IEmployeeStorage>();
+            _mockEmployeeController = new EmployeeController(_mockStorage.Object);
+
+        }
 
         [Test]
         public void DeleteEmployee_WhenIdPassed_InteractsWithStorage()
         {
             // arrange
-            const int id = 1;
-            var mockStorage = new Mock<IEmployeeStorage>();
-            var mockEmployeeController = new EmployeeController(mockStorage.Object);
+            // was done in setup
 
             // act
-            mockEmployeeController.DeleteEmployee(id);
+            _mockEmployeeController.DeleteEmployee(_empId);
 
             // assert
             // we only need to check for interaction with storage object here
             // no need to check the actual deletion - because deletion is responsibility of storage not employee controller 
-            mockStorage.Verify(x => x.DeleteEmployee(id));
+            _mockStorage.Verify(x => x.DeleteEmployee(_empId));
+        }
+
+        [Test]
+        public void DeleteEmployee_WhenIdPassed_ReturnsActionResult()
+        {
+            // arrange
+            // was done in setup
+
+            // act
+            var actual = _mockEmployeeController.DeleteEmployee(_empId);
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual, Is.InstanceOf<ActionResult>());
+            Assert.That(actual, Is.InstanceOf<RedirectResult>());
         }
     }
 }
