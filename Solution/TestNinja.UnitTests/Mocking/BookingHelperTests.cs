bae9d7877fd64cd4eeb50f72241dee6406c22a25
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-24 18:33:44 +0530

    mocking a queryable

--- Solution/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ Solution/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -12,7 +12,7 @@ namespace TestNinja.UnitTests.Mocking
         [SetUp]
         public void SetUp()
         {
-            _booking = new Booking();
+            _booking = new Booking { Id = 1 };
             _bookingRepositoryMock = new Mock<IBookingRepository>();
         }
 
@@ -33,8 +33,50 @@ namespace TestNinja.UnitTests.Mocking
         }
 
         [Test]
-        public void OverlappingBookingsExist_BookingWithOverlapExists_ReturnsReference()
+        public void OverlappingBookingsExist_BookingWithOverlapExists_ReturnsFirstReference()
         {
+            // arrange
+            _booking.Status = "Active";
+            _booking.ArrivalDate = new DateTime(2000, 1, 5);
+            _booking.DepartureDate = new DateTime(2000, 1, 8);
+            var bookings = new List<Booking>
+            {
+                new()
+                {
+                    Id = _booking.Id,
+                    Status = "Active",
+                    ArrivalDate = _booking.DepartureDate.AddDays(-1),
+                    DepartureDate = _booking.ArrivalDate.AddDays(1),
+                    Reference = "Ref1"
+                },
+                new()
+                {
+                    Id = _booking.Id,
+                    Status = "Active",
+                    ArrivalDate = _booking.DepartureDate.AddDays(-1),
+                    DepartureDate = _booking.ArrivalDate.AddDays(1),
+                    Reference = "Ref2"
+                },
+                new()
+                {
+                    Id = _booking.Id,
+                    Status = "Active",
+                    ArrivalDate = _booking.DepartureDate,
+                    DepartureDate = _booking.ArrivalDate,
+                    Reference = "Ref3"
+                },
+            };
+            var queryable = bookings.AsQueryable();
+            _bookingRepositoryMock.Setup(x => x.FindActiveBookings(_booking.Id))
+                .Returns(queryable);
+
+            // act
+            var actual = BookingHelper.OverlappingBookingsExist(_booking,
+                _bookingRepositoryMock.Object);
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual, Is.EqualTo("Ref1"));    // first bookings reference
         }
 
         [Test]
